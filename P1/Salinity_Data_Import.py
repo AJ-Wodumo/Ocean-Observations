9dafca464cd5c00f37cefe2977a1465828db920b
AJ-Wodumo <160007686+AJ-Wodumo@users.noreply.github.com>
2024-02-20 14:37:53 +0200

    Add files via upload

---
+++ P1/Salinity_Data_Import.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 13:18:43 2024
+
+@author: student
+"""
+
+import pandas as pd
+
+
+
+import pandas as pd
+
+salinity_data = 'Salinity_Data.txt'
+salinity = pd.read_csv(salinity_data)
+print(salinity)
